1287daa0384e9025ddd5d58ce17a4fb1584257cd
erick <michel172002@gmail.com>
2023-10-22 01:05:29 -0300

    feat: using FormGroup and FormControlName

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,26 +1,32 @@
 import { ListarPensamentoComponent } from './../listar-pensamento/listar-pensamento.component';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css'],
 })
-export class CriarPensamentoComponent {
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
+export class CriarPensamentoComponent implements OnInit{
 
-  constructor(private service: PensamentoService, private router: Router) {}
+  constructor(private service: PensamentoService, private router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulario reativo'],
+      autoria: ['Erick'],
+      modelo: ['modelo2']
+    })
+  }
+
+  formulario!: FormGroup;
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamentos'])
+    this.service.criar(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamentos']);
     });
   }
 }
